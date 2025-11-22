2706b96e54c0ddf1a4986f30a893b4c2e68e458a
Conor Fabian <110637682+conorfabian@users.noreply.github.com>
2025-11-21 20:59:17 -0800

    Time: 4 ms (74.06%), Space: 12.9 MB (68.66%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -8,9 +8,10 @@ class Solution(object):
         l, r = 0, len(numbers) - 1
 
         while l < r:
-            if numbers[l] + numbers[r] < target:
-                l += 1
-            elif numbers[l] + numbers[r] > target:
+            val = numbers[l] + numbers[r]
+            if val > target:
                 r -= 1
+            elif val < target:
+                l += 1
             else:
-                return [l + 1, r + 1]
+                return [l+1, r+1]
