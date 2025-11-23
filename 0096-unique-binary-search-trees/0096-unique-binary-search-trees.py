bf087852a4ad3b5eec4b0a875943c772eff61a57
Conor Fabian <110637682+conorfabian@users.noreply.github.com>
2025-11-22 16:15:52 -0800

    Time: 0 ms (100%), Space: 12.5 MB (52.51%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in xrange(1, n+1):
+            for j in xrange(i):
+                res[i] += res[j] * res[i-1-j]
+        return res[n]
+    
+    def numTrees(self, n):
+        return math.factorial(2*n)/(math.factorial(n)*math.factorial(n+1))
