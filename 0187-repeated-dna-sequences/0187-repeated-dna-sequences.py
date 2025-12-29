3ab3d4ab3ac078e7e091aa0bbff1b9c61bedf75b
Conor Fabian <110637682+conorfabian@users.noreply.github.com>
2025-12-29 13:04:03 -0800

    Time: 43 ms (23.6%), Space: 32 MB (20.39%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        sequences = collections.defaultdict(int) #set '0' as the default value for non-existing keys
+        for i in range(len(s)):
+            sequences[s[i:i+10]] += 1#add 1 to the count
+        return [key for key, value in sequences.iteritems() if value > 1]
